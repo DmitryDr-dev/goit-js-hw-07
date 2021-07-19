85115c1c4464d15e203f0f111339e42ede25213f
DmitryDr-dev <dmitrydr.dev@gmail.com>
2021-07-19 23:41:17 +0300

    8 задача

--- js/task-8.js
+++ js/task-8.js
@@ -16,3 +16,55 @@
 // </div>
 
 // <div id="boxes"></div>
+
+// const refs = {
+//   inputEl: document.querySelector('input'),
+//   btnRender: document.querySelector('[data-action="render"]'),
+//   btnDestroy: document.querySelector('[data-action="destroy"]'),
+// };
+const [inputEl, btnRender, btnDestroy] =
+  document.querySelector('#controls').children;
+const boxesEl = document.querySelector('#boxes');
+
+function createRandom() {
+  return `rgb(${(Math.random() * 255) << 0}, ${(Math.random() * 255) << 0}, ${
+    (Math.random() * 255) << 0
+  })`;
+}
+
+const createElements = () => {
+  let elements = [];
+  for (let i = 0; i < inputEl.value; i += 1) {
+    let size = `${30 + i * 10}px`;
+    let color = createRandom();
+    let element = { size, color };
+    elements.push(element);
+  }
+
+  return elements;
+};
+
+const createMarkUp = options => {
+  return options.map(({ size, color }) => {
+    const divEl = document.createElement('div');
+    divEl.style.width = size;
+    divEl.style.height = size;
+    divEl.style.backgroundColor = color;
+    // console.log(divEl);
+    return divEl;
+  });
+};
+
+const renderMarkUp = el => {
+  const elements = createElements();
+  const markUp = createMarkUp(elements);
+  boxesEl.append(...markUp);
+};
+
+const destroyMarkup = () => {
+  inputEl.value = '';
+  boxesEl.innerHTML = '';
+};
+
+btnRender.addEventListener('click', renderMarkUp);
+btnDestroy.addEventListener('click', destroyMarkup);
