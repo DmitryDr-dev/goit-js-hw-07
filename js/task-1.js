736938dffb2383e961b4d39c733d66db54f527d5
DmitryDr-dev <dmitrydr.dev@gmail.com>
2021-07-19 17:15:25 +0300

    Task 2 Completed

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 // Напиши скрипт, который выполнит следующие операции.
-// 1. Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
 // Например для первой категории получится:
 // Категория: Животные
@@ -13,5 +13,10 @@ const categoriesList = document.querySelectorAll('li.item');
 console.log(`В списке ${categoriesList.length} категории.`);
 
 categoriesList.forEach(category => {
-  console.log(category.querySelector('h2'));
+  const categoryTitle = category.querySelector('h2');
+  const categoryItems = category.querySelectorAll('li');
+
+  console.log(
+    `Категория: ${categoryTitle.textContent} \n Количество элементов: ${categoryItems.length}`,
+  );
 });
--- js/task-2.js
+++ js/task-2.js
@@ -11,4 +11,28 @@
 //   'Зелень',
 //   'Приправы',
 // ];
+
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const listEl = document.querySelector('#ingredients');
+
+const createListFromArray = array => {
+  return array.map(ingredient => {
+    const listElement = document.createElement('li');
+    listElement.textContent = ingredient;
+    return listElement;
+  });
+};
+
+const elementsToAdd = createListFromArray(ingredients);
+
+listEl.append(...elementsToAdd);
